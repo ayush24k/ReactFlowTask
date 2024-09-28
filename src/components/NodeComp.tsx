34b4d1f53fcf6bab3408fe95475a172847cf494d
ayush24k <ayushanshu69@gmail.com>
2024-09-28 19:48:46 +0530

    added node delete button

--- src/components/NodeComp.tsx
+++ src/components/NodeComp.tsx
@@ -1,9 +1,15 @@
-import { Handle, Position } from "@xyflow/react";
+import { Handle, Position, useReactFlow } from "@xyflow/react";
 
-export default function NodeComp({data}:any) {
+export default function NodeComp({ data, id }: any) {
+    const {setNodes} = useReactFlow();
     return (
         <div className="bg-slate-200 px-3 p-1 border-[1px] border-black rounded-sm">
-            <p>{data.name}</p>
+            <div className="flex gap-3">
+                <p>{data.name}</p>
+                <button className="text-red-600 hover:text-red-500" onClick={() => {
+                    setNodes(prevNodes => prevNodes.filter((node) => node.id !== id))
+                }}>x</button>
+            </div>
             <Handle type="source" position={Position.Right} />
         </div>
     )
--- src/components/Workflow.tsx
+++ src/components/Workflow.tsx
@@ -1,4 +1,4 @@
-import { addEdge, Background, Connection, Controls, ReactFlow, useEdgesState, useNodesState } from "@xyflow/react";
+import { addEdge, Background, Connection, Controls, MiniMap, ReactFlow, useEdgesState, useNodesState } from "@xyflow/react";
 import { useCallback, useState } from "react";
 import { initialEdges, initialNodes } from "./workflow.constants";
 import '@xyflow/react/dist/style.css';
@@ -11,8 +11,11 @@ export default function Workflow() {
     const [nodes, setNodes, onNodeChange] = useNodesState(initialNodes);
     const [edges, setEdges, onEdgeChange] = useEdgesState(initialEdges);
 
+    //node cordinates
+    
+
     const onConnect = useCallback((connection: Connection) => {
-        const edge = { ...connection, animated: true, id: `${edges.length + 1}`,};
+        const edge = { ...connection, animated: true, id: `${edges.length + 1}`, };
         //@ts-ignore
         setEdges((prevEdges) => addEdge(edge, prevEdges));
     }, [setEdges])
@@ -30,15 +33,16 @@ export default function Workflow() {
                         <input type="text" className="rounded-sm p-1 text-slate-900" placeholder="enter node name" onChange={(e) => setNodeName(e.target.value)} />
 
                         <button className="bg-blue-600 rounded-sm p-1" onClick={() => {
-                            const cordinates = Math.random() * 200;
+                            const xCordinates = Math.random() * 300;
+                            const yCordinates = Math.random() * 600;
                             setNodes((prevNodes) => [...prevNodes, {
                                 id: `${prevNodes.length + 1}`,
-                                data: {name: nodeName},
+                                data: { name: nodeName },
                                 type: "nodeComp",
-                                position: {x: cordinates, y:cordinates},
+                                position: { x: xCordinates, y: yCordinates },
                                 hidden: false
                             }])
-                            setEdges((prevEdges)=> [...prevEdges, {
+                            setEdges((prevEdges) => [...prevEdges, {
                                 id: `${prevEdges.length + 1}`,
                                 source: `${nodes.length + 1}`,
                                 target: 'bignode',
@@ -48,19 +52,22 @@ export default function Workflow() {
                     </div>
 
                     <button className="bg-blue-700 rounded-sm p-1 mt-3" onClick={() => {
+                        const xCordinates = Math.random() * 800;
+                        const yCordinates = Math.random() * 600;
                         setNodes((prevNodes) => [...prevNodes, {
                             id: `bignode`,
                             data: {},
                             type: "bigNodeComp",
-                            position: {x: 0, y: 100},
+                            position: { x: xCordinates, y: yCordinates },
                             hidden: false
                         }])
                     }}>Add Biiig Node</button>
                 </div>
             </div>
             <div className="h-[720px] w-[1280px] bg-slate-100 text-black">
-                <ReactFlow nodes={nodes} edges={edges} onNodesChange={onNodeChange} onEdgesChange={onEdgeChange} onConnect={onConnect} nodeTypes={nodeTypes} fitView>
+                <ReactFlow nodes={nodes} edges={edges} onNodesChange={onNodeChange} onEdgesChange={onEdgeChange} onConnect={onConnect} nodeTypes={nodeTypes}>
                     <Controls />
+                    <MiniMap />
                     <Background />
                 </ReactFlow>
             </div>
